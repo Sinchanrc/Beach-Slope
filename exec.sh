2aef7c9f15460c4890cc9ec7ae7168530e392e86
Sinchan Roy Chowdhury <sinchanrc90@gmail.com>
2023-10-28 22:59:09 +0530

    Changing exec

--- exec.sh
+++ exec.sh
@@ -2,6 +2,7 @@
 
 #EXPORTING OPENMP ENVIRONMENT VARIABLES
 export OMP_NUM_THREADS=12
+export MKL_NUM_THREADS=12
 export OMP_SCHEDULE="dynamic"
 # export OMP_STACKSIZE=16M
 # export OMP_PROC_BIND="close"
@@ -15,12 +16,27 @@ export OMP_SCHEDULE="dynamic"
 #COMPILING SOURCE FILES#
 
 #GNU COMPILERS
-gfortran-12 -ffree-line-length-512 -Ofast -fopenmp -march=native -mtune=native -std=f2008 -c -flto=auto \
--fimplicit-none -funroll-loops -frepack-arrays -fno-realloc-lhs -mavx2 \
-particle.f90 interactions.f90 transfer.f90 domain.f90 initialize.f90 kernel.f90 functions.f90 \
+
+
+gfortran-12 -ffree-line-length-512 -Ofast -fprefetch-loop-arrays -fopenmp -march=native -mtune=native \
+-mavx2 -c -funroll-loops -flto=auto -ftree-vectorize -fstack-arrays \
+-m64  -I"${MKLROOT}/include" \
+initialize.f90
+
+
+gfortran-12 -ffree-line-length-512 -Ofast -fprefetch-loop-arrays -fopenmp -march=native -mtune=native \
+-mavx2 -std=f2008 -c -funroll-loops -flto=auto -ftree-vectorize -fstack-arrays \
+particle.f90 interactions.f90 transfer.f90 domain.f90 kernel.f90 functions.f90 \
 search.f90 gradient.f90 setup.f90 solver.f90 boundary.f90 output.f90 internal.f90 \
-memory.f90 isph.f90 integrator.f90 turbulence.f90 part_shift.f90 mls.f90 \
-scalar.f90 probe.f90
+memory.f90 isph.f90 integrator.f90 turbulence.f90 part_shift.f90 \
+mls.f90 scalar.f90
+
+# gfortran-12 -ffree-line-length-512 -Ofast -fopenmp -march=native -mtune=native -std=f2008 -c -flto=auto \
+# -fimplicit-none -funroll-loops -frepack-arrays -fno-realloc-lhs -mavx2 \
+# particle.f90 interactions.f90 transfer.f90 domain.f90 initialize.f90 kernel.f90 functions.f90 \
+# search.f90 gradient.f90 setup.f90 solver.f90 boundary.f90 output.f90 internal.f90 \
+# memory.f90 isph.f90 integrator.f90 turbulence.f90 part_shift.f90 mls.f90 \
+# scalar.f90 probe.f90
 
 # ifort  -Ofast -qopenmp -std03 -ipo -xHost -c  \
 # particle.f90 interactions.f90 transfer.f90 domain.f90 initialize.f90 kernel.f90 functions.f90 \
@@ -31,12 +47,23 @@ scalar.f90 probe.f90
 #COMPILING EXECUTABLE#
 
 #GNU COMPILERS
-gfortran-12 -ffree-line-length-512 -Ofast -fopenmp -march=native -mtune=native -std=f2008 -flto=auto \
--fimplicit-none -funroll-loops -frepack-arrays -fno-realloc-lhs -mavx2 \
+
+gfortran-12 -ffree-line-length-512 -Ofast -fprefetch-loop-arrays -fopenmp -march=native -mtune=native \
+-mavx2 -std=f2008 -funroll-loops -flto=auto -ftree-vectorize -fstack-arrays \
+-L${MKLROOT}/lib/intel64 -Wl,--no-as-needed -lmkl_gf_lp64 -lmkl_gnu_thread \
+-lmkl_core -lgomp -lpthread -lm -ldl \
 particle.o interactions.o transfer.o domain.o initialize.o kernel.o functions.o \
 search.o gradient.o setup.o solver.o boundary.o output.o internal.o \
-memory.o isph.o integrator.o turbulence.o part_shift.o scalar.o mls.o \
-probe.o main.f90 -o main
+memory.o isph.o integrator.o turbulence.o part_shift.o \
+mls.o scalar.o main.f90 -o main
+
+
+# gfortran-12 -ffree-line-length-512 -Ofast -fopenmp -march=native -mtune=native -std=f2008 -flto=auto \
+# -fimplicit-none -funroll-loops -frepack-arrays -fno-realloc-lhs -mavx2 \
+# particle.o interactions.o transfer.o domain.o initialize.o kernel.o functions.o \
+# search.o gradient.o setup.o solver.o boundary.o output.o internal.o \
+# memory.o isph.o integrator.o turbulence.o part_shift.o scalar.o mls.o \
+# probe.o main.f90 -o main
 
 # ifort  -Ofast -qopenmp -std03 -ipo -xHost \
 # particle.o interactions.o transfer.o domain.o initialize.o kernel.o functions.o \
